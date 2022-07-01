9b211a64358eb33f4afcc659a32f94febad975cf
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-01 21:05:57 +0200

    feat(styles): add global scss
    Add basic global.scss to only set box-sizing for now. More is comming.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 
 import Layout from '../components/UI/layout/layout';
 
+import '../styles/global.scss';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
