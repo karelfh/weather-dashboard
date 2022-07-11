ed22994eb1fcdfc173752447cea230af411f9c6c
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-11 22:27:47 +0200

    feat(styles): add weather icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/UI/layout/layout';
 
 import '../styles/global.scss';
+import '../styles/icons/weather-icons.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
