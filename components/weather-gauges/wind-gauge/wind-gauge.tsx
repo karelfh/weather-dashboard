8b3d786104337b31604ba039fc454c316f20ccb8
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-14 20:04:29 +0200

    fix(components): wind gauge shows right direction
    Wind gauge show the right direction in cardinal directions (eg. NE).

--- components/weather-gauges/wind-gauge/wind-gauge.tsx
+++ components/weather-gauges/wind-gauge/wind-gauge.tsx
@@ -1,11 +1,18 @@
 import style from './wind-gauge.module.scss';
 
 const WindGauge = ({ windDirection }: { windDirection?: number }) => {
+	const getDirection = (angle: number) => {
+		let directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+		let index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
+
+		return directions[index];
+	};
+
 	return (
 		<div className={style['compass']}>
 			<div className={style['direction']}>
 				<p>
-					NE
+					{windDirection && `${getDirection(windDirection)}`}
 					<span>{windDirection}&deg;</span>
 				</p>
 			</div>
