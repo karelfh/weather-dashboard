8dede510fc9a770f4afb351239773be3a9c31051
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-16 10:14:22 +0200

    fix(styles): hamburger menu hover on mobile
    Remove hover effect on hamburger menu icon. On Mobile phone it stayed in
    the hover state after it was clicked.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Poppins:wght@300;400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
