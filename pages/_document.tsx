a56e1dbeae112f2790a9f642ce51e462c4cd140a
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-17 11:57:43 +0200

    build(manifest): add favicon and manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import Favicon from '../components/UI/favicon/favicon';
+
 const Document = () => {
 	return (
 		<Html lang="en">
 			<Head>
+				<Favicon />
+				<link rel="manifest" href="/site.webmanifest" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Poppins:wght@300;400;700&display=swap"
 					rel="stylesheet"
