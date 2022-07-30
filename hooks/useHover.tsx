7f96333b78f56c4f8d40e5382fffd04d60d56a5b
Karel F. Houf <karel.frantisek.houf@gmail.com>
2022-07-30 19:09:59 +0200

    feat(hooks): useHover custom hook

---
+++ hooks/useHover.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useHover = <T,>(): [React.MutableRefObject<T>, boolean] => {
+	const [value, setValue] = useState<boolean>(false);
+
+	const ref: any = useRef<T | null>(null);
+
+	const handleMouseOver = (): void => setValue(true);
+	const handleMouseOut = (): void => setValue(false);
+
+	useEffect(() => {
+		const node: any = ref.current;
+
+		if (node) {
+			node.addEventListener('mouseover', handleMouseOver);
+			node.addEventListener('mouseout', handleMouseOut);
+
+			return () => {
+				node.removeEventListener('mouseover', handleMouseOver);
+				node.removeEventListener('mouseout', handleMouseOut);
+			};
+		}
+	}, []);
+
+	return [ref, value];
+};
+
+export default useHover;
